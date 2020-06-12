7cbae094d2b73e893413839c7bba1ea521792c1e
Rsja12 <asocarras10@gmail.com>
2020-06-12 11:00:32 -0400

    make comment form a controlled form

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 
 export class CommentBox extends Component {
+
+    state = {
+        comment: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            comment: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+
+        // TODO - Call action creator to post comment
+
+        this.setState({
+            comment: ''
+        })
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h4>Add A Comment</h4>
-                <textarea />
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.handleChange}
+                />
                 <div>
                     <button>Submit Comment</button>
                 </div>
