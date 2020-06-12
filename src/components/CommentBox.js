afd177bcf68f3331cd4da9135d63596724c4b10f
Rsja12 <asocarras10@gmail.com>
2020-06-12 10:55:12 -0400

    refactor Comment box component into class component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentBox = () => {
-    return (
-        <div>
-            Comment Box
-        </div>
-    )
+export class CommentBox extends Component {
+    render() {
+        return (
+            <form>
+                <h4>Add A Comment</h4>
+                <textarea />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        )
+    }
 }
 
 export default CommentBox
