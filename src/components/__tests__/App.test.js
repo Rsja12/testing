4ea62c8b22605e4df3bf9994d5dde682e141c595
Rsja12 <asocarras10@gmail.com>
2020-06-12 10:50:50 -0400

    refactor App.test.js to use beforeEach func

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,15 +5,18 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-it('renders a comment box', () => {
-    const wrapper = shallow(<App />)
+let wrapper;
 
+beforeEach(() => {
+    wrapper = shallow(<App />)
+})
+
+it('renders a comment box', () => {
     // .find method returns array of all instances of CommentBox
     //  we want to only have one instance
     expect( wrapper.find(CommentBox).length ).toEqual(1);
 })
 
 it('renders a comment list', () => {
-    const wrapper = shallow(<App />)
     expect( wrapper.find(CommentList).length ).toEqual(1)
 })
