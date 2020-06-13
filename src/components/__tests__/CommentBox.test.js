7db41783eaade5a59b7e614205487d63cd7814ce
Rsja12 <asocarras10@gmail.com>
2020-06-12 20:13:17 -0400

    create reducers directory

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,24 +18,24 @@ it('has a textarea and a button', () => {
     expect( wrapper.find('button').length ).toEqual(1)
 })
 
-it('has a textarea that users can type in', () => {
-    wrapper.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
-    })
-    // we need it because setState is asynchronous
-    wrapper.update()
+describe('The textarea', () => {
 
-    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
-})
-
-it('clears textarea on form submit', () => {
-    wrapper.find('textarea').simulate('change', {
-        target: { value: 'comment' }
+    beforeEach(() => {
+        wrapper.find('textarea').simulate('change', {
+            target: { value: 'comment' }
+        })
+        // we need .update() because setState is asynchronous
+        wrapper.update()
     })
-    wrapper.update()
-    expect(wrapper.find('textarea').prop('value')).toEqual('comment')
+
+    it('has a textarea that users can type in', () => {
+        expect(wrapper.find('textarea').prop('value')).toEqual('comment')
+    })
     
-    wrapper.find('form').simulate('submit')
-    wrapper.update()
-    expect(wrapper.find('textarea').prop('value')).toEqual('')
+    it('clears textarea on form submit', () => {
+        wrapper.find('form').simulate('submit')
+        wrapper.update()
+        expect(wrapper.find('textarea').prop('value')).toEqual('')
+    })
+
 })
