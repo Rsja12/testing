249720ce8ca2be45ae62ca7fb4d575df265b2d06
Rsja12 <asocarras10@gmail.com>
2020-06-12 20:32:40 -0400

    refactor action creator and commentsReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
-import { SAVE_COMMENT } from 'save_comment'
 
 export function saveComment(comment) {
     return {
-        type: SAVE_COMMENT,
+        type: 'SAVE_COMMENT',
         payload: comment
     }
 }
