c9ffc95034c729a8a602ddf922faff28b1939c9d
Rsja12 <asocarras10@gmail.com>
2020-06-16 15:35:00 -0400

    refactor reducers to return comments from last to first

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -4,10 +4,10 @@ export default function(state = [], action) {
 
     switch( action.type ) {
         case 'SAVE_COMMENT':
-            return [...state, action.payload]
+            return [action.payload, ...state]
         case 'FETCH_COMMENTS':
             const comments = action.payload.data.map( comment => comment.title )
-            return [...state, ...comments]
+            return [...comments, ...state]
         default:
             return state
     }
