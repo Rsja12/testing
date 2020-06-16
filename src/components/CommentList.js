6b13be35c238d67b570288f7ac5905f006ff872b
Rsja12 <asocarras10@gmail.com>
2020-06-16 11:26:13 -0400

    refactor commentsList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const CommentList = () => {
-    return (
-        <div>
-            Comment List
-        </div>
-    )
+export class CommentList extends Component {
+
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return(
+                <li key={comment}>
+                    {comment}
+                </li>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default CommentList
+const mapStateToProps = state => ({
+    comments: state.comments
+})
+
+export default connect(mapStateToProps)(CommentList)
