84aa646a0bd33b7bde1c6e07a2925fc68645bbb4
Rsja12 <asocarras10@gmail.com>
2020-06-17 11:01:14 -0400

    refactor app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import CommentList from 'components/CommentList'
 import CommentBox from 'components/CommentBox'
 
-const App = () => {
-    return (
-        <div>
-            <Route exact path='/' component={CommentBox} />
-            <Route exact path='/posts' component={CommentList}/>
-        </div>
-    )
+class App extends Component {
+
+    renderBtn() {
+        if (this.props.auth) {
+            return(
+                <button>Sign Out</button>
+            )
+        }
+
+        return(
+            <button>
+                Sign In
+            </button>
+        )
+    }
+
+    renderHeader() {
+        return (
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/posts'>Post Comment</Link>
+                </li>
+                <li>
+                    { this.renderBtn() }
+                </li>
+            </ul>
+        )
+    }
+
+    render(){
+        return (
+            <div>
+                {this.renderHeader()}
+                <Route exact path='/' component={CommentList} />
+                <Route exact path='/posts' component={CommentBox}/>
+            </div>
+        )
+    }
 }
 
-export default App
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(App)
